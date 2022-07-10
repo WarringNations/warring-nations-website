e9101eab7de4fb6623f4a2b91233af98a6e2481f
WarringNations <quentin.drouet109@gmail.com>
2022-07-10 18:07:44 +0200

    responsive menu

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ let toggle = document.querySelector('.toggle')
 let body = document.querySelector('body')
 
 toggle.addEventListener('click', () => {
-    body.classList.toggle('open-menu')
+    body.classList.toggle('open')
 })
